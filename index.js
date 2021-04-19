4e775b351842194fe4689f5099658920a2fad76b
ricardozv <ricardosilvanet03@gmail.com>
2021-04-19 13:29:48 -0300

    up code

---
+++ index.js
@@ -0,0 +1,13 @@
+require ('dotenv').config;
+const ccxt = require('ccxt');
+const axios = require('axios');
+
+const tick = async() => {
+
+}
+
+const run = () => {
+    const config = {
+        asset:
+    }
+}
